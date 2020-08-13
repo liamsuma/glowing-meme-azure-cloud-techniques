2197857e6e25feb979a6a6b1c132b1e33c60bd4a
Su Li <61469229+liamsuma@users.noreply.github.com>
2020-08-13 09:15:33 -0400

    Create blob-storage-upload.py
    For a detailed explanation, please see the following link on SO: https://stackoverflow.com/questions/58900507/upload-and-delete-azure-storage-blob-using-azure-storage-blob-or-azure-storage

---
+++ blob-storage-upload.py
@@ -0,0 +1,25 @@
+# Script to upload csv files into Blob Storage 
+# Current script is using BlobServiceClient with changes made by Azure since May 2019 and going forward 
+
+import os
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+
+try: 
+    # Please do not change connect_str unless stated otherwise 
+    connect_str = '<connection string>'
+    container_name = 'ocr' # already created in Azure 
+    upload_file_path = '/path/to/your/input/files' # input file path 
+    output_blob_name = 'output_blob.csv' # what we view in container 
+
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=output_blob_name)
+    
+    print("\nUploading to Azure Storage as blob:\n\t" + output_blob_name)
+
+    with open(upload_file_path, 'rb') as data:
+        blob_client.upload_blob(data=data)
+
+    
+except Exception as ex:
+    print('Exception:')
+    print(ex)
